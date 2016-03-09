26d4dd74d6ca02adec56f639ed049e5865757f86
SayliS <rssaylis@gmail.com>
2016-03-09 15:50:48 +0200

    Added LocalFileStorageRepository validations and removed file extension logic

--- src/FileStorage/LocalFileStorageRepository.cs
+++ src/FileStorage/LocalFileStorageRepository.cs
@@ -8,16 +8,28 @@ namespace FileStorage
 {
     public class LocalFileStorageRepository : IFileStorageRepository
     {
-        private string storageFolder;
-        private Dictionary<string, IFileFormat> formats;
+        string storageFolder;
+        Dictionary<string, IFileFormat> formats;
 
         public LocalFileStorageRepository(string storageFolder)
         {
+            if (string.IsNullOrWhiteSpace(storageFolder))
+                throw new ArgumentNullException(nameof(storageFolder));
+
             Initialize(storageFolder);
         }
 
         public void Upload(string fileName, byte[] data, List<FileMeta> metaInfo, string format = "original")
         {
+            if (string.IsNullOrWhiteSpace(fileName))
+                throw new ArgumentNullException(nameof(fileName));
+
+            if (ReferenceEquals(data, null) == true)
+                throw new ArgumentNullException(nameof(data));
+
+            if (ReferenceEquals(metaInfo, null) == true)
+                throw new ArgumentNullException(nameof(metaInfo));
+
             var filePath = Path.Combine(storageFolder, format, fileName);
 
             var fileInfo = new FileInfo(filePath);
@@ -30,6 +42,17 @@ namespace FileStorage
 
         public LocalFile Download(string fileName, string format = "original")
         {
+            if (formats.ContainsKey(format) == false)
+                throw new NotSupportedException($"This file format is not supported. {format}");
+
+            if (string.IsNullOrWhiteSpace(fileName))
+                throw new ArgumentNullException(nameof(fileName));
+
+            var formatInstance = formats[format];
+
+            if (formatInstance.FindFile(fileName) == false)
+                formatInstance.Generate(fileName);
+
             var uri = this.GetFileUri(fileName, format);
 
             if (string.IsNullOrEmpty(uri))
@@ -46,55 +69,53 @@ namespace FileStorage
 
         public string GetFileUri(string fileName, string format = "original")
         {
+            if (formats.ContainsKey(format) == false)
+                throw new NotSupportedException($"This file format is not supported. {format}");
+
+            if (string.IsNullOrWhiteSpace(fileName))
+                throw new ArgumentNullException(nameof(fileName));
+
             var formatInstance = formats[format];
 
-            if (ReferenceEquals(null, formatInstance) == false)
-            {
-                if (!formatInstance.FindFile(fileName))
-                {
-                    if (formatInstance.Generate(fileName) == false)
-                    {
-                        return string.Empty;
-                    }
-                }
-            }
-            else
-            {
-                throw new NotSupportedException(string.Format("This file format is not supported. {0}", format));
-            }
+            if (formatInstance.FindFile(fileName) == false)
+                formatInstance.Generate(fileName);
 
             var directoryPath = Path.Combine(storageFolder, format);
             DirectoryInfo directoryInfo = new DirectoryInfo(directoryPath);
             FileInfo[] files = directoryInfo.GetFiles();
-            var found = files.SingleOrDefault(x => x.Name.ToLowerInvariant().Replace(x.Extension.ToLowerInvariant(), string.Empty) == fileName.ToLowerInvariant());
+            var found = files.SingleOrDefault(x => x.Name.ToLowerInvariant() == fileName.ToLowerInvariant());
+
+            if (ReferenceEquals(found, null) == true)
+                return string.Empty;
 
             return found.FullName;
         }
 
         public bool FileExists(string fileName, string format = "original")
         {
+            if (string.IsNullOrWhiteSpace(fileName))
+                throw new ArgumentNullException(nameof(fileName));
+
             var directoryPath = Path.Combine(storageFolder, format);
             DirectoryInfo directoryInfo = new DirectoryInfo(directoryPath);
             FileInfo[] files = directoryInfo.GetFiles();
-            var found = files.SingleOrDefault(x => x.Name.ToLowerInvariant().Replace(x.Extension.ToLowerInvariant(), string.Empty) == fileName.ToLowerInvariant());
+            var found = files.SingleOrDefault(x => x.Name.ToLowerInvariant() == fileName.ToLowerInvariant());
 
             return found != null;
         }
 
         public byte[] Generate(byte[] data, string format)
         {
+            if (ReferenceEquals(data, null) == true)
+                throw new ArgumentNullException(nameof(data));
+
+            if (formats.ContainsKey(format) == false)
+                throw new NotSupportedException($"This file format is not supported. {format}");
+
             var formatInstance = formats[format];
+            var newData = formatInstance.Generate(data);
 
-            if (ReferenceEquals(null, formatInstance) == false)
-            {
-                var newData = formatInstance.Generate(data);
-
-                return newData;
-            }
-            else
-            {
-                throw new NotSupportedException(string.Format("This file format is not supported. {0}", format));
-            }
+            return newData;
         }
 
         private void Initialize(string storageFolder)
