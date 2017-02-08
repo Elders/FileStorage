309d98be881c2e7d417a991838dd6f7d03081721
Sinstraliz <blagovest.vp@gmail.com>
2017-02-07 19:21:40 -0600

    Revert build script.

--- build.cmd
+++ build.cmd
@@ -63,6 +63,9 @@ SET DESCRIPTION_AZURE="Azure FileStorage"
 SET DESCRIPTION_FILESYSTEM="FileSystem FileStorage"
 SET DESCRIPTION_WEBAPI="FileStorage Web API integration"
 
+%FAKE% %NYX% appName=FileStorage.AmazonS3 appReleaseNotes=%RELEASE_NOTES% appSummary=%SUMMARY_S3% appDescription=%DESCRIPTION_S3% nugetPackageName=FileStorage.AmazonS3 nugetkey=%RELEASE_NUGETKEY%
+%FAKE% %NYX% appName=FileStorage.Azure appReleaseNotes=%RELEASE_NOTES% appSummary=%SUMMARY_AZURE% appDescription=%DESCRIPTION_AZURE% nugetPackageName=FileStorage.Azure nugetkey=%RELEASE_NUGETKEY%
+%FAKE% %NYX% appName=FileStorage.FileSystem appReleaseNotes=%RELEASE_NOTES% appSummary=%SUMMARY_FILESYSTEM% appDescription=%DESCRIPTION_FILESYSTEM% nugetPackageName=FileStorage.FileSystem nugetkey=%RELEASE_NUGETKEY%
 %FAKE% %NYX% appName=FileStorage.WebApi appReleaseNotes=%RELEASE_NOTES% appSummary=%SUMMARY_WEBAPI% appDescription=%DESCRIPTION_WEBAPI% nugetPackageName=FileStorage.WebApi nugetkey=%RELEASE_NUGETKEY%
 
 IF NOT [%1]==[] (%FAKE% %NYX% "target=Release" -st appReleaseNotes=%RELEASE_NOTES%)
