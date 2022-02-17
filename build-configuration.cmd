3d3442b1669933aa08875b7b57cf8a2a2bb3236e
Paulina-Dyulgerska <paylina_st@yahoo.com>
2022-02-17 15:06:56 +0200

    BREAKING CHANGE: CI / Switches to azure-pipelines and sem-rel from appveyor

--- build-configuration.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-
-@powershell -File .nyx\build.ps1 '--appname=Elders.FileStorage' '--nugetPackageName=FileStorage.Abstractions'
-@powershell -File .nyx\build.ps1 '--appname=Elders.FileStorage.Azure' '--nugetPackageName=FileStorage.Azure'
